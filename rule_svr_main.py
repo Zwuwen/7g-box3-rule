ea2dc0f8db1e3de81bb758b88567dd28e442e0ca
王海泉 <wanghaiquan@my7g.cn>
2021-01-13 16:49:36 +0800

    修改猴子补丁导致nameko服务调用异常的bug。

--- rule_svr_main.py
+++ rule_svr_main.py
@@ -5,7 +5,6 @@ cur_dir = os.getcwd()
 pre_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
 sys.path.append(cur_dir)
 sys.path.append(pre_dir)
-import eventlet
 from nameko_svr.rule_nameko import RuleNameko
 from rule.rule_mng import RuleMng
 from config import RULE_LOG_PATH, RULE_LOG_LEVEL
@@ -13,15 +12,14 @@ from log.log import MyLog
 from rpc_call.client import DevCall
 
 MyLog.init(is_enable_std=False, log_period = 7, level = RULE_LOG_LEVEL, path = RULE_LOG_PATH)
-eventlet.monkey_patch(all = False, os =True, select=True, socket=True, thread=True, time=True)
 def main():
     msg = MyLog.color_green('+++++++++规则引擎启动+++++++++')
     MyLog.logger.info(msg)
+    RuleNameko.open()
     while not DevCall.mng_srv_ready():
         time.sleep(1)
     msg = MyLog.color_green('管理服务已就就绪')
     MyLog.logger.info(msg)
-    RuleNameko.open()
     RuleMng.timer_rule_decision()
     while True:
         time.sleep(3600)
